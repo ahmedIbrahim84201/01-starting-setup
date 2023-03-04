58991cc15c3cb6c7e931c3c6684bf116464ae780
Ahmed Ibrahim <105105189+ahmedIbrahim84201@users.noreply.github.com>
2023-03-04 12:34:05 +0300

    Last commit
    add More expenses and Bars

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-
 
 function ExpenseItem(props) {
   //we use props when we r receiving data from another play and its not pre defined
 
   return (
+    <li>
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
@@ -14,6 +15,7 @@ function ExpenseItem(props) {
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </Card>
+    </li>
   );
 }
 export default ExpenseItem;
